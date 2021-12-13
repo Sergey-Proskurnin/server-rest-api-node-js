14708b38239899a6538f4e41fd056ac77bec4c20
Sergey-Proskurnin <proskurnin@ua.fm>
2021-12-14 01:58:25 +0200

    Adds privacy-policy

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const router = Router();
+const path = require("path");
 
 router.use('/users', require('./users'));
 router.use('/contacts', require('./contacts'));
+router.use('/privacy-policy', (_req, res) => {
+      res.sendFile(path.join(__dirname, '../../public/privacy-policy.html'));
+    });
 
 module.exports = router;
